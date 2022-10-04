599ee60a9fb6af7c06fbca9c4e224387abef925f
Yeshin Lee <yeslee5004@gmail.com>
2022-10-04 18:46:28 +0900

    frontend setup

---
+++ src/public/js/app.js
@@ -0,0 +1,4 @@
+// frontend code
+// although edit code here, don't run nodemon
+
+alert("Hello World");
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express, { application } from "express";
 
 const app = express();
+console.log("dir: ", __dirname);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-console.log("hello");
-
-app.listen(3000);
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
