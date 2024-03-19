a8ec470d7e2b4f8768789e8821732d9585fc6e59
yeslee-v <yeslee5004@gmail.com>
2024-03-19 17:19:07 +0900

    Add nickname form

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,28 +1,47 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 
 // 'WebSocket': The URL's scheme must be either 'ws' or 'wss'. 'http' is not allowed.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const modifyJsonToString = (type, payload) => {
+  const msg = { type, payload }
+
+  return JSON.stringify(msg)
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server!");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New Message: ", message.data);
+  const li = document.createElement("li");
+
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server!!");
 });
 
-const handleSubmit = (event) => {
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+
+  const input = nicknameForm.querySelector("input");
+
+  socket.send(modifyJsonToString("nickname", input.value));
+};
+
+const handleMessageSubmit = (event) => {
   event.preventDefault();
 
   const input = messageForm.querySelector("input");
 
-  socket.send(input.value);
+  socket.send(modifyJsonToString("newMessage", input.value));
   input.value = "";
 };
 
-messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
+messageForm.addEventListener("submit", handleMessageSubmit);
