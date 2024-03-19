440c4659e8f53c9e4eb9771370afad6c089fa73b
Yeshin Lee <yeslee5004@gmail.com>
2024-03-19 12:17:40 +0900

    Install ws

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express, { application } from "express";
 
 const app = express();
@@ -10,4 +12,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+const handleListen = () => console.log("Listening on http://localhost:3000");
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, handleListen);
