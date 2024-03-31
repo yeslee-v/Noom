524c583762e57cd801df31ce1b9e65c20764a1a8
Yeshin Lee <yeslee5004@gmail.com>
2024-03-31 21:59:42 +0900

    Add Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -15,7 +16,17 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+  mode: "development",
+});
 
 const getPublicRooms = () => {
   const { sids, rooms } = wsServer.sockets.adapter;
